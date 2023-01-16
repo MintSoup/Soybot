7237d1ccf82c5e7444327a727df1b6faa526ed61
Areg <aregak2005@gmail.com>
2023-01-16 10:44:56 +0400

    update

--- soybot.py
+++ soybot.py
@@ -23,19 +23,23 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 fontsize = 100
-fnt = ImageFont.truetype("/usr/share/fonts/gnu-free/FreeMono.otf", fontsize)
+fnt = ImageFont.truetype("FreeMono.otf", fontsize)
 
 
 class MyClient(discord.Client):
-
 	async def on_ready(self):
 		print("Logged in and ready")
 
 	def getfiles(self):
-		return sorted([f[:-4] for f in listdir(None) if isfile(f) and f.endswith(".png")])
+		return sorted(
+			[f[:-4] for f in listdir(None) if isfile(f) and f.endswith(".png")]
+		)
 
-	async def soy(self, wojak, text, message, ref):
+	async def soy(self, wojak, text, message: discord.Message, ref):
 		if not wojak in self.getfiles():
 			return
 
@@ -47,22 +51,30 @@ class MyClient(discord.Client):
 		toffset = len(lines) * fontsize / 2
 		for line in lines:
 			w, _ = draw.textsize(line, font=fnt)
-			draw.text((img.width * 0.69 - w / 2, img.height / 2 - toffset + offset), line, 0, font=fnt)
+			draw.text(
+				(img.width * 0.69 - w / 2, img.height / 2 - toffset + offset),
+				line,
+				0,
+				font=fnt,
+			)
 			offset += fontsize
 
-		filename = "/tmp/" + \
-			''.join(random.choices(string.ascii_uppercase + string.digits, k=16)) + ".png"
+		filename = (
+			"/tmp/"
+			+ "".join(random.choices(string.ascii_uppercase + string.digits, k=16))
+			+ ".png"
+		)
 		img.save(filename)
 		f = open(filename, "rb")
 
 		if ref is None:
 			await message.channel.send(file=discord.File(f, filename=wojak + ".png"))
 		else:
-			await message.channel.send(file=discord.File(f, filename=wojak + ".png"), reference=ref)
+			await message.channel.send(
+				file=discord.File(f, filename=wojak + ".png"), reference=ref
+			)
 			f.close()
 			remove(filename)
-
-
 
 	async def on_message(self, message: discord.Message):
 		if message.author == self.user:
@@ -73,13 +85,12 @@ class MyClient(discord.Client):
 			for f in self.getfiles():
 				s += f + "\n"
 				print(s)
-			s += "```"
-			await message.channel.send(s)
+				s += "```"
+				await message.channel.send(s)
 			return
 
 		if not message.content.startswith(">"):
 			return
-
 
 		wojak = message.content[1:].split(" ")[0]
 
@@ -87,7 +98,7 @@ class MyClient(discord.Client):
 		replied = None
 
 		if " " in message.content:
-			text = message.clean_content[1 + len(wojak):]
+			text = message.clean_content[1 + len(wojak) :]
 		elif message.reference is None:
 			async for msg in message.channel.history(limit=10):
 				if msg == message:
@@ -101,7 +112,8 @@ class MyClient(discord.Client):
 
 		await self.soy(wojak, text, message, replied)
 
-client = MyClient()
+
+client = MyClient(intents=intents)
 f = open("token", "r")
 token = f.read()
 f.close()
